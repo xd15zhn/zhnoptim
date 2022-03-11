ef674de09e6fb5e9012b9bfb6f3fa31340f3eb3c
xd15zhn <yz37zhn@163.com>
2022-03-11 20:19:10 +0800

    03.11 2018 laptop

--- algorithm.hpp
+++ algorithm.hpp
@@ -1,5 +1,5 @@
-#ifndef __ALGORITHM_H
-#define __ALGORITHM_H
+#ifndef __ZHNOPTIM_H
+#define __ZHNOPTIM_H
 #include <iostream>
 #include <vector>
 #include <random>
@@ -45,35 +45,6 @@ protected:
     std::vector<double> _BestSolution;
 };
 
-/***********************
-Algorithm initialize
-*@ solution: Initial solution vector
-*@ solvelen: Length of solution vector
-**********************/
-inline Algorithm::Algorithm(const std::vector<double>& solution, int solvelen)
-{
-    ASSERT_ERROR_ALG(solvelen>0, "Length of solution vector should be greater than or equal to 1!");
-    _solvelen = solvelen;
-    for (int i=0; i<_solvelen && i<(int)solution.size(); ++i)
-        _BestSolution.push_back(solution[i]);
-    _IterateCnt = 0; _TermIterate = 1e4;
-    _MinCost = INFINITE; _TermCost = 1;
-    TermType = COUNT;
-    std::cout.precision(8);
-}
-
-inline void Algorithm::Set_TerminationConditions(double mincost, int maxiterate, TermCriteria type)
-{
-    _TermCost = mincost; _TermIterate = maxiterate; TermType = type;
-}
-
-inline void Algorithm::Solution_Print() const
-{
-    std::cout<<_MinCost<<": ";
-    for (int i=0; i<_solvelen; ++i)
-        std::cout<<_BestSolution[i]<<", ";
-    std::cout<<std::endl;
-}
 
 /***********************
 Differential Evolution(DE/best/2)
@@ -112,4 +83,4 @@ private:
 };
 
 NAMESPACE_ZHNOPTIM_R
-#endif
+#endif  // ZHNOPTIM
---
+++ zhnoptim.cpp
@@ -0,0 +1,31 @@
+#include "ZHNOPTIM.hpp"
+
+/***********************
+Algorithm initialize
+*@ solution: Initial solution vector
+*@ solvelen: Length of solution vector
+**********************/
+Algorithm::Algorithm(const std::vector<double>& solution, int solvelen)
+{
+    ASSERT_ERROR_ALG(solvelen>0, "Length of solution vector should be greater than or equal to 1!");
+    _solvelen = solvelen;
+    for (int i=0; i<_solvelen && i<(int)solution.size(); ++i)
+        _BestSolution.push_back(solution[i]);
+    _IterateCnt = 0; _TermIterate = 1e4;
+    _MinCost = INFINITE; _TermCost = 1;
+    TermType = COUNT;
+    std::cout.precision(8);
+}
+
+void Algorithm::Set_TerminationConditions(double mincost, int maxiterate, TermCriteria type)
+{
+    _TermCost = mincost; _TermIterate = maxiterate; TermType = type;
+}
+
+void Algorithm::Solution_Print() const
+{
+    std::cout<<_MinCost<<": ";
+    for (int i=0; i<_solvelen; ++i)
+        std::cout<<_BestSolution[i]<<", ";
+    std::cout<<std::endl;
+}
